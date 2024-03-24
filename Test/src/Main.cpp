f62bf891bc9584b86f8f29b5ed5f136c5ae31b45
GMDG <gmdg141995@gmail.com>
2024-03-24 14:52:33 +0100

    Added a random placement of obstacles

--- Test/src/Main.cpp
+++ Test/src/Main.cpp
@@ -5,8 +5,8 @@
 #include <cmath>
 
 // Grid size macro
-#define GRID_HEIGHT 2000
-#define GRID_WIDTH 2000
+#define GRID_HEIGHT 200
+#define GRID_WIDTH 200
 
 enum AlgorithmTypes
 {
@@ -42,6 +42,83 @@ int main()
 {
 	std::cout << "Running Pathfinding on a " << GRID_HEIGHT << " x " << GRID_WIDTH << " grid" << std::endl;
 
+	srand(time(NULL));
+	//srand(13);
+	Grid grid(GRID_HEIGHT, GRID_WIDTH);
+	for (int i = 0; i < GRID_HEIGHT; i++)
+	{
+		for (int j = 0; j < GRID_WIDTH; j++)
+		{
+			if (rand() % 100 < 25)
+			{
+				grid.PlaceObstacle(Grid::Point(i, j));
+			}
+		}
+	}
+
+	// GRID OBSTACLES
+	//grid.PlaceObstacle(Grid::Point(1, 4));
+	//grid.PlaceObstacle(Grid::Point(1, 5));
+	//grid.PlaceObstacle(Grid::Point(2, 5));
+	//grid.PlaceObstacle(Grid::Point(3, 5));
+	//grid.PlaceObstacle(Grid::Point(4, 5));
+	//grid.PlaceObstacle(Grid::Point(5, 5));
+	//grid.PlaceObstacle(Grid::Point(6, 5));
+	//grid.PlaceObstacle(Grid::Point(6, 4));
+	//grid.PlaceObstacle(Grid::Point(9, 7));
+	//grid.PlaceObstacle(Grid::Point(8, 7));
+	//grid.PlaceObstacle(Grid::Point(7, 7));
+	//grid.PlaceObstacle(Grid::Point(6, 7));
+	//grid.PlaceObstacle(Grid::Point(9, 7));
+	//grid.PlaceObstacle(Grid::Point(9, 6));
+	//grid.PlaceObstacle(Grid::Point(9, 5));
+	//grid.PlaceObstacle(Grid::Point(16, 16));
+	//grid.PlaceObstacle(Grid::Point(16, 15));
+	//grid.PlaceObstacle(Grid::Point(16, 14));
+	//grid.PlaceObstacle(Grid::Point(17, 13));
+	//grid.PlaceObstacle(Grid::Point(17, 14));
+	//grid.PlaceObstacle(Grid::Point(18, 13));
+	//grid.PlaceObstacle(Grid::Point(19, 13));
+	//grid.PlaceObstacle(Grid::Point(7, 4));
+	//grid.PlaceObstacle(Grid::Point(7, 5));
+	//grid.PlaceObstacle(Grid::Point(10, 4));
+	//grid.PlaceObstacle(Grid::Point(10, 5));
+	//grid.PlaceObstacle(Grid::Point(10, 6));
+	//grid.PlaceObstacle(Grid::Point(10, 3));
+	//grid.PlaceObstacle(Grid::Point(10, 2));
+	//grid.PlaceObstacle(Grid::Point(0, 5));
+	//grid.PlaceObstacle(Grid::Point(16, 13));
+	//grid.PlaceObstacle(Grid::Point(16, 12));
+	//grid.PlaceObstacle(Grid::Point(16, 11));
+	//grid.PlaceObstacle(Grid::Point(16, 10));
+	//grid.PlaceObstacle(Grid::Point(16, 9));
+	//grid.PlaceObstacle(Grid::Point(15, 12));
+	//grid.PlaceObstacle(Grid::Point(13, 12));
+	//grid.PlaceObstacle(Grid::Point(14, 12));
+	//grid.PlaceObstacle(Grid::Point(12, 12));
+	//grid.PlaceObstacle(Grid::Point(11, 12));
+	//grid.PlaceObstacle(Grid::Point(10, 12));
+	//grid.PlaceObstacle(Grid::Point(9, 12));
+	//grid.PlaceObstacle(Grid::Point(9, 15));
+	//grid.PlaceObstacle(Grid::Point(8, 15));
+	//grid.PlaceObstacle(Grid::Point(7, 15));
+	//grid.PlaceObstacle(Grid::Point(10, 15));
+	//grid.PlaceObstacle(Grid::Point(11, 15));
+	//grid.PlaceObstacle(Grid::Point(6, 15));
+	//grid.PlaceObstacle(Grid::Point(5, 15));
+	//grid.PlaceObstacle(Grid::Point(12, 15));
+	//grid.PlaceObstacle(Grid::Point(13, 15));
+
+
+	// Conversion from a GRID to a GRAPH since the algorithm requires the latter
+	std::shared_ptr<Graph> graph = Utility::GridToGraph(&grid);
+
+	//graph->Print();
+
+	Graph::Node start = graph->GetNodes()->at(0);
+	Graph::Node end = graph->GetNodes()->at((GRID_HEIGHT * GRID_WIDTH) - 1);
+
+
 	for (int algType = AlgorithmTypes::ESimpleAStar; algType <= AlgorithmTypes::EPriorityQueueAStar; algType++)
 	{
 		{
@@ -50,72 +127,7 @@ int main()
 			//PROFILE();
 			//PROFILE_MEMORY();
 
-			// Grid creation
-			Grid grid(GRID_HEIGHT, GRID_WIDTH);
-
-			// GRID OBSTACLES
-			//grid.PlaceObstacle(Grid::Point(1, 4));
-			//grid.PlaceObstacle(Grid::Point(1, 5));
-			//grid.PlaceObstacle(Grid::Point(2, 5));
-			//grid.PlaceObstacle(Grid::Point(3, 5));
-			//grid.PlaceObstacle(Grid::Point(4, 5));
-			//grid.PlaceObstacle(Grid::Point(5, 5));
-			//grid.PlaceObstacle(Grid::Point(6, 5));
-			//grid.PlaceObstacle(Grid::Point(6, 4));
-			//grid.PlaceObstacle(Grid::Point(9, 7));
-			//grid.PlaceObstacle(Grid::Point(8, 7));
-			//grid.PlaceObstacle(Grid::Point(7, 7));
-			//grid.PlaceObstacle(Grid::Point(6, 7));
-			//grid.PlaceObstacle(Grid::Point(9, 7));
-			//grid.PlaceObstacle(Grid::Point(9, 6));
-			//grid.PlaceObstacle(Grid::Point(9, 5));
-			//grid.PlaceObstacle(Grid::Point(16, 16));
-			//grid.PlaceObstacle(Grid::Point(16, 15));
-			//grid.PlaceObstacle(Grid::Point(16, 14));
-			//grid.PlaceObstacle(Grid::Point(17, 13));
-			//grid.PlaceObstacle(Grid::Point(17, 14));
-			//grid.PlaceObstacle(Grid::Point(18, 13));
-			//grid.PlaceObstacle(Grid::Point(19, 13));
-			//grid.PlaceObstacle(Grid::Point(7, 4));
-			//grid.PlaceObstacle(Grid::Point(7, 5));
-			//grid.PlaceObstacle(Grid::Point(10, 4));
-			//grid.PlaceObstacle(Grid::Point(10, 5));
-			//grid.PlaceObstacle(Grid::Point(10, 6));
-			//grid.PlaceObstacle(Grid::Point(10, 3));
-			//grid.PlaceObstacle(Grid::Point(10, 2));
-			//grid.PlaceObstacle(Grid::Point(0, 5));
-			//grid.PlaceObstacle(Grid::Point(16, 13));
-			//grid.PlaceObstacle(Grid::Point(16, 12));
-			//grid.PlaceObstacle(Grid::Point(16, 11));
-			//grid.PlaceObstacle(Grid::Point(16, 10));
-			//grid.PlaceObstacle(Grid::Point(16, 9));
-			//grid.PlaceObstacle(Grid::Point(15, 12));
-			//grid.PlaceObstacle(Grid::Point(13, 12));
-			//grid.PlaceObstacle(Grid::Point(14, 12));
-			//grid.PlaceObstacle(Grid::Point(12, 12));
-			//grid.PlaceObstacle(Grid::Point(11, 12));
-			//grid.PlaceObstacle(Grid::Point(10, 12));
-			//grid.PlaceObstacle(Grid::Point(9, 12));
-			//grid.PlaceObstacle(Grid::Point(9, 15));
-			//grid.PlaceObstacle(Grid::Point(8, 15));
-			//grid.PlaceObstacle(Grid::Point(7, 15));
-			//grid.PlaceObstacle(Grid::Point(10, 15));
-			//grid.PlaceObstacle(Grid::Point(11, 15));
-			//grid.PlaceObstacle(Grid::Point(6, 15));
-			//grid.PlaceObstacle(Grid::Point(5, 15));
-			//grid.PlaceObstacle(Grid::Point(12, 15));
-			//grid.PlaceObstacle(Grid::Point(13, 15));
-
-			// Conversion from a GRID to a GRAPH since the algorithm requires the latter
-			std::shared_ptr<Graph> graph = Utility::GridToGraph(&grid);
-
-			//graph->Print();
-
-			Graph::Node start = graph->GetNodes()->at(0);
-			Graph::Node end = graph->GetNodes()->at((GRID_HEIGHT * GRID_WIDTH) - 1);
-
 			// Call the A-Star algorithm that returns a vector of Connections that go from the START node to the END node of the Graph
-
 			std::shared_ptr<std::vector<Graph::Connection>> shortestPath;
 
 			switch (algType)
